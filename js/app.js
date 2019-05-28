7f5bc5bd3237563696e69e285b0e91fd28e28d46
Jerry Doran <Jerry.Doran@pccairfoils.com>
2019-05-28 12:19:51 -0400

    Finished Gradma's Sweets Project

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
-const cartInfo = document.getElementById('cart-info');
-
-cartInfo.addEventListener('click', () => {
-  const cart = document.getElementById('show-cart');
-  cart.classList.toggle('show-cart');
-  console.log(cart);
-});
+const cartInfo = document
+  .getElementById('cart-info')
+  .addEventListener('click', () => {
+    const cart = document.getElementById('cart');
+    cart.classList.toggle('show-cart');
+  });
